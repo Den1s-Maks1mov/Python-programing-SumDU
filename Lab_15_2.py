aadcd20c607110e812da02969e8e3f6542d43d6c
Den1s-Maks1mov <maksymov.denys@student.sumdu.edu.ua>
2024-11-10 19:02:18 +0200

    Lab_15.py (update)

--- Lab_15_2.py
+++ Lab_15_2.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# список місяців
+months = [
+    "Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень",
+    "Липень", "Серпень", "Вересень", "Жовтень", "Листопад", "Грудень"
+]
+
 # завантаження даних з файлу
 data = pd.read_csv('Lab15_2_data.csv')
 
@@ -12,13 +18,13 @@ monthly_data = data.groupby('Month')['Total'].sum()
 
 # знаходження найбільш популярного місяця
 most_popular_month = monthly_data.idxmax()
-print(f"Найбільш популярний місяць для велосипедистів у 2015 році: {most_popular_month}")
+print(f"Найбільш популярний місяць для велосипедистів у 2015 році: {months[most_popular_month - 1]}")
 
 # побудова графіку використання велодоріжок по місяцях
 plt.figure(figsize=(10, 6))
-monthly_data.plot(kind='bar', color='skyblue')
-plt.title('Використання велодоріжок по місяцях у 2015 році')
-plt.xlabel('Місяць')
-plt.ylabel('Загальна кількість велосипедистів')
-plt.xticks(rotation=0)
+plt.bar(months, monthly_data)
+plt.title("Динаміка за місяцями")
+plt.xlabel("Місяці")
+plt.ylabel("Значення")
+plt.xticks(rotation=45)
 plt.show()
